6412356c05c92ed3fc6099ccd0f40581dcfb8ce2
Dan Hutchinson <danielhutchinson5@gmail.com>
2019-04-09 20:55:01 +0100

    fix: tslint errors

--- src/components/hello-world/HelloWorld.ts
+++ src/components/hello-world/HelloWorld.ts
@@ -1,6 +1,6 @@
-import { Component, Vue } from "vue-property-decorator";
+import { Component, Vue } from 'vue-property-decorator';
 
 @Component
 export default class HelloWorld extends Vue {
-  public message: string = "Hello World!";
+  public message: string = 'Hello World!';
 }
