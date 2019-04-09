509430cb882974f71b7711c7f6b41a114c191ac8
Dan Hutchinson <danielhutchinson5@gmail.com>
2019-04-09 20:52:31 +0100

    refactor: stripped app down to hello world
    Vue is new to me I don't know what I am doing

---
+++ src/components/hello-world/HelloWorld.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="hello">
+    <h1>{{message}}</h1>
+  </div>
+</template>
+
+<script lang="ts" src="./HelloWorld.ts"></script>
+<style scoped lang="scss" src="./HelloWorld.scss"></style>
